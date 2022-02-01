6024b87d42fdec0410775da32ba7f8e97b912683
chai2010 <chaishushan@gmail.com>
2022-02-02 00:33:57 +0800

    添加代码

---
+++ builtin/builtin.go
@@ -0,0 +1,32 @@
+package builtin
+
+import _ "embed"
+
+//go:embed builtin_darwin_amd64.ll
+var llBuiltin_darwin_amd64 string
+
+func GetBuiltinLL(goos, goarch string) string {
+	switch goos {
+	case "darwin":
+		switch goarch {
+		case "amd64":
+			return llBuiltin_darwin_amd64
+		}
+	case "linux":
+	case "windows":
+	}
+	panic("builtin missing")
+}
+
+const Header = `
+declare i32 @ugo_builtin_println(i32)
+declare i32 @ugo_builtin_exit(i32)
+`
+
+const MainMain = `
+define i32 @main() {
+	call i32() @ugo_main_init()
+	call i32() @ugo_main_main()
+	ret i32 0
+}
+`
